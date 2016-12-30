d915a652a10711fff7444e2ad1f0e224f5351fca
Michael Billimoria <Michael.Billimoria@ssu.local>
2016-12-31 10:05:50 +1100

    Concatenated JavaScript Files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    concat = require('gulp-concat');
 
-gulp.task('log', function() {
-  gutil.log('Workflows are awesome');
+var jsSources = ['components/scripts/*.js'];
+
+gulp.task('js', function () {
+  gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('builds/development/js'))
 });
