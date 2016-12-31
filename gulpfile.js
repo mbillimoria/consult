171e318fefda039e5fdb7ee19412155271433bbc
Michael Billimoria <Michael.Billimoria@ssu.local>
2016-12-31 15:14:15 +1100

    Added live reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,36 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     concat = require('gulp-concat'),
+    connect = require('gulp-connect'),
     browserify = require('gulp-browserify');
 
 var jsSources = ['components/scripts/*.js'];
+var htmlSources = ['builds/development/*.html'];
+
 
 gulp.task('js', function() {
   gulp.src(jsSources)
     .pipe(concat('script.js'))
     .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'))
+    .pipe(connect.reload())
+});
+
+gulp.task('html', function() {
+  gulp.src(htmlSources)
+    .pipe(connect.reload())
 });
 
 gulp.task('watch', function() {
   gulp.watch(jsSources, ['js']);
+  gulp.watch(htmlSources, ['html']);
 });
 
-gulp.task('default', ['js', 'watch']);
+gulp.task('connect', function() {
+  connect.server({
+    root: 'builds/development/',
+    livereload: true
+  });
+});
+
+gulp.task('default', ['html', 'js', 'connect', 'watch']);
